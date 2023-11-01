80e0fafce058f5230a1b690dfd30ba8303494ed2
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-01 23:46:31 +0200

    name

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,13 +6,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
             Console.Clear();
-            Console.WriteLine("Hello World");
+            Console.WriteLine("What is your name?");
 
             string name = Console.ReadLine();
             Console.WriteLine(name);
+            Console.WriteLine($"Hello {name}");
         }
     }
 }
