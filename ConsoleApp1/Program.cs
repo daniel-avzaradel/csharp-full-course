25de5e151c249d299fb4f28608cf15ad9f214d34
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-01 22:45:46 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+            Console.WriteLine("Hello World");
+        }
+    }
+}
