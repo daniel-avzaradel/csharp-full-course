e30da8f9fba7ab4ae231ef18c61b120420f3c343
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-04 22:22:44 +0200

    string methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,10 +12,15 @@ namespace ConsoleApp1
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.DarkGreen;
 
-            string randomStr = "daniel avzaradel";
+            string randomStr = "daniel_avzaradel";
             Console.WriteLine("String Length: {0}", randomStr.Length);
             Console.WriteLine("String Contains is: {0}", randomStr.Contains("ani"));
             Console.WriteLine("Index of 'n' is: {0}", randomStr.IndexOf("n"));
+
+            Console.WriteLine("Uppercase: {0}", randomStr.ToUpper());
+            Console.WriteLine("Lowercase: {0}", randomStr.ToLower());
+            Console.WriteLine("Pad Left: {0}", randomStr.PadLeft(20, '.'));
+            Console.WriteLine("Pad Right: {0}", randomStr.PadRight(20, '.'));
         }
     }
 }
