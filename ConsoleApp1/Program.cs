2f92a5eab4ad44afc4f2e114c0801922446687e1
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-04 20:40:25 +0200

    using currencies format

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -6,40 +7,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Default;
+
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.DarkGreen;
 
-            bool boolFromString = bool.Parse("true");
-            int intFromString = int.Parse("100");
-            double doubleFromStr = double.Parse("1.2345");
-            string strValue = doubleFromStr.ToString();
-
-            Console.WriteLine("Converted boolean from string: {0}", boolFromString);
-            Console.WriteLine("Converted int from string: {0}", intFromString);
-            Console.WriteLine(doubleFromStr);
-            Console.WriteLine(strValue);
-
-            Console.WriteLine($"Data type: {strValue.GetType()}");
-            Console.WriteLine($"Data type: {doubleFromStr.GetType()}");
-            Console.WriteLine($"Data type: {intFromString.GetType()}");
-            Console.WriteLine($"Data type: {boolFromString.GetType()}");
-
-            Console.WriteLine("What is your first name?");
-            string firstName = Console.ReadLine();
-
-            Console.WriteLine("What is your last name?");
-            string lastName = Console.ReadLine();
-
-            Console.WriteLine("My first name is: {0}", firstName);
-            Console.WriteLine("My last name is: {0}", lastName);
-            Console.WriteLine("My full name is: {0} {1}", firstName, lastName);
-
-            double dblNum = 12.3727;
-            Console.WriteLine($"Integer: {(int)dblNum}");
-
-            int intNum = 10;
-            long longNum = intNum;
-            Console.WriteLine(longNum);
+            Console.WriteLine("Currency: {0:c}", 23.455);
+            Console.WriteLine("Pad With Zeros Currency: {0:d4}", 23);
+            Console.WriteLine("3 Decimals Currency: {0:f3}", 23.615253);
+            Console.WriteLine("Commas Currency: {0:n4}", 22000);
         }
     }
 }
